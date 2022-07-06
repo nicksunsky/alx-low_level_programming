8533c26294822d3af5f8b30e474a31f90524554a
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-07-06 22:31:59 +0300

    Update and rename 0-putchar.c to 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+  	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+int main(void)
+{
+print_alphabet;
+}
