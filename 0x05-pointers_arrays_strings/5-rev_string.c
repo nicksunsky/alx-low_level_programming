4d8a212ca7abbda43fec59c1791cba368e5af4f4
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-07-12 22:25:37 +0300

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - check the code 
+ * @s: String to be reversed
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+
+	i--;
+
+	for (j = 0; j <= i; j++, i--)
+	{
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+	}
+}
