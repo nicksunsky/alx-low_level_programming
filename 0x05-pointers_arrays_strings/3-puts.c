c667cb7c66e061dae393fec5ae85ccafc22fd303
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-07-12 22:23:29 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - check the code
+ * @str: String to be printed out
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
