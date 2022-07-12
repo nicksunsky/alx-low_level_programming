3b86a83e7aa6abbb967ce98729185b1afcf4d214
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-07-12 22:19:38 +0300

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * swap_int - check the code 
+ * @a: First variable to be swapped
+ * @b: Second variable to be swapped
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
