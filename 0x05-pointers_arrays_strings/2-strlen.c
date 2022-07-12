7530de33a6e6219189659024d0d7576fa79a4a82
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-07-12 22:22:11 +0300

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code 
+ * @s: Inputted string pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
