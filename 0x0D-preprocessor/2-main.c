4d6cd177cbdaa3cc1f7a9425923cb0cc3c556f45
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-08-01 15:33:31 +0300

    Initial commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print name of file main was compiled from
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf(__FILE__"\n");
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
