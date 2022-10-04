c0da5c85254be050934d380d6aae23180ffc5b38
nicksunsky <nicksunsky@gmail.com>
2022-10-04 05:32:49 -0700

    Add files

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncpy - copys str from src to dest
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to copy
+ *
+ * Return: pointer to dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (n > 0)
+	{
+		dest[i] = src[i];
+		if (src[i] == '\0')
+			break;
+		n--;
+		i++;
+	}
+	if (n > 0)
+		while (n)
+		{
+			dest[i] = '\0';
+			i++;
+			n--;
+		}
+
+	return (dest);
+}
